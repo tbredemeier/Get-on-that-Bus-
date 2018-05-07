3dc8f683d378218d8038a9f48d481a92e12af100
Tom Bredemeier <tbredemeier@barrington220.org>
2018-05-06 20:09:53 -0500

    ability to display current location on a map

--- Get on that Bus!/ViewController.swift
+++ Get on that Bus!/ViewController.swift
@@ -7,17 +7,21 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
